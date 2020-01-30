17b88c033afd655eb1f9fef92509f2730a8eb6af
abousamraa <web2manag@gmail.com>
2020-01-31 00:41:04 +0200

    add project files

---
+++ src/server.ts
@@ -0,0 +1,59 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { filterImageFromURL, deleteLocalFiles } from './util/util';
+
+(async () => {
+
+  // Init the Express application
+  const app = express();
+
+  // Set the network port
+  const port = process.env.PORT || 8082;
+
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
+  app.get("/filteredimage", async (req, res) => {
+    const imgUrl: string = req.query.image_url;
+
+    // checking imgUrl is exist
+    if (!imgUrl) {
+      return res.status(400).send({
+        message: "please provide image url "
+      });
+    }
+    
+    // checking imgUrl is valid
+    var request = require('request');
+    request(imgUrl, function (error: any, response: any, body: any) {
+      if (response.statusCode == 404 || ((response.headers["content-type"]).match(/(image)+\//g)) == null) {
+        return res.status(400).send({
+          message: "please provide valid image url "
+        });
+      }
+    }
+    );
+
+    try {
+      console;
+      const filteredImageFromURL: string = await filterImageFromURL(imgUrl);
+      res.sendFile(filteredImageFromURL, () =>
+        deleteLocalFiles([filteredImageFromURL])
+      );
+    } catch (error) {
+      res.sendStatus(422).send("can not process this image ");
+    }
+  });
+
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get("/", async (req, res) => {
+    res.send("try GET /filteredimage?image_url={{}}")
+  });
+
+
+  // Start the Server
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
+})();
